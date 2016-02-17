dde33d26bee6c0b41348dc91c644a9e7effc71d0
bouletator <clement.romanet@gmail.com>
2016-02-17 11:38:29 +0100

    premier commit

---
+++ src/main/java/dcll/crom/App.java
@@ -0,0 +1,13 @@
+package dcll.crom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
